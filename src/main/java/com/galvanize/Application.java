0a5b8ae7981075bd64f76e2b69808e5293af638a
Dora Xia <duruoxia1992@gmail.com>
2022-07-12 14:08:55 -0700

    finished CLI exercise

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,7 +1,14 @@
 package com.galvanize;
 
+
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = args[0];
+        String email = args[1];
+        System.out.println(name  + " <" + email + ">");
     }
+
+
 }
+
